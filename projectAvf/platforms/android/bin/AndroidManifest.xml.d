0ab9b205743c3f09f0d0b5ce61fb2596acc4af0c
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-11-07 22:02:50 -0500

    Added Week 2 updates for API deployment to iOS and Android devices

--- projectAvf/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Project31312/Desktop/AVF-TERM1311/projectAvf/platforms/android/bin/AndroidManifest.xml : \
-/Users/Project31312/Desktop/AVF-TERM1311/projectAvf/platforms/android/AndroidManifest.xml \
